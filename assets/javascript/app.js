2536229fff2007e5015524a910d3426018e28e79
Dina Khouri <dina.khouri.91@gmail.com>
2019-01-21 13:17:52 -0600

    populates gif containers, links are console logging correctly but there is a problem with displaying them

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,11 @@
 //scroll on hover for the gif result
-
 //scroll-right
 $(".arrow-right").mouseover(function() {
   event.preventDefault();
   console.log("hover");
-  //   $(".arrow-right").animate({
-  //     width: 60
-  //   });
 
   $(".Gif-result").animate({ scrollLeft: "+=200" }, 500);
 });
-
-// $(".arrow-right").mouseout(function() {
-//   event.preventDefault();
-
-//   $(".arrow-right").animate({
-//     width: 50
-//   });
-//});
 
 //scroll-left
 $(".arrow-left").mouseover(function() {
@@ -29,14 +17,6 @@ $(".arrow-left").mouseover(function() {
 
   $(".Gif-result").animate({ scrollLeft: "-=200" }, 500);
 });
-
-// $(".arrow-left").mouseout(function() {
-//   event.preventDefault();
-
-//   $(".arrow-left").animate({
-//     width: 50
-//   });
-// });
 
 //zoom in and out on hover for any gif in the gifresult
 $(".Gif-container").mouseover(function() {
@@ -63,3 +43,66 @@ $(".Gif-favs").mouseover(function() {
       }
     );
 });
+
+// start the actual code
+//define preset topics
+var topics = ["kids", "pets", "brownies", "friends", "sports"];
+
+// "//api.giphy.com/v1/gifs/search?api_key=2lF9sKMxxHfZIY0plsOMnxN63wpaqwix&q=" +
+//   topic +
+//   "&limit=10&offset=0&rating=G&lang=en";
+
+function displayTopics() {
+  var newTopic = $(this).attr("data-name");
+  var queryURL =
+    "https:////api.giphy.com/v1/gifs/search?api_key=2lF9sKMxxHfZIY0plsOMnxN63wpaqwix&q=" +
+    newTopic +
+    "&limit=10&offset=0&rating=G&lang=en";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $(".Gif-result").empty();
+    console.log("clicked");
+    console.log(response.data[0].images.original.url);
+    for (i = 0; i < response.data.length; i++) {
+      console.log(response.data[i].images.original.url);
+      $(".Gif-result").prepend(
+        "<div class='Gif-container'><img class='img-fluid Gif-Individual' src='" +
+          response.data[i].images.original.url +
+          "></div>"
+      );
+    }
+  });
+}
+
+// Function for displaying topic data
+function renderButtons() {
+  $(".Initial-buttons").empty();
+
+  // Looping through the array of topics
+  for (var i = 0; i < topics.length; i++) {
+    var a = $("<button>");
+    a.addClass("btn btn-secondary Initial-button");
+    a.attr("data-name", topics[i]);
+    a.text(topics[i]);
+    $(".Initial-buttons").append(a);
+  }
+}
+
+// function handles for adding topic to buttons
+$("#search-Gif").on("click", function(event) {
+  event.preventDefault();
+  var topic = $("#Gif-input")
+    .val()
+    .trim();
+
+  topics.push(topic);
+  renderButtons();
+});
+
+// Adding a click event listener to all elements with a class of "Initial-button"
+$(document).on("click", ".Initial-button", displayTopics);
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
